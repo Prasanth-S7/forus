494fff924ed5f849cca78864b57cd29193c07558
Prasanth-S7 <prasanthsampath2005@gmail.com>
2024-07-25 13:48:49 +0530

    fixed broken table

--- client/src/components/custom-component/tableAddedGoods.jsx
+++ client/src/components/custom-component/tableAddedGoods.jsx
@@ -9,54 +9,10 @@ import {
     TableRow,
   } from "@/components/ui/table"
   
-  const invoices = [
-    {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      totalAmount: "$250.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      totalAmount: "$150.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      totalAmount: "$350.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      totalAmount: "$450.00",
-      paymentMethod: "Credit Card",
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      totalAmount: "$550.00",
-      paymentMethod: "PayPal",
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      totalAmount: "$200.00",
-      paymentMethod: "Bank Transfer",
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      totalAmount: "$300.00",
-      paymentMethod: "Credit Card",
-    },
-  ]
-  
   export function TableAddedGoods({goods}) {
+    goods = Object.values(goods)
     console.log(goods)
-    return (
+      return (
       <Table>
         <TableCaption>A list of goods that has been added</TableCaption>
         <TableHeader>
@@ -71,9 +27,9 @@ import {
           {goods.map((good,index) => (
             <TableRow key={index}>
               <TableCell className="font-medium">{index+1}</TableCell>
-              <TableCell>{good.name}</TableCell>
-              <TableCell>{good.itemCode}</TableCell>
-              <TableCell className="text-right">{good.quantity}</TableCell>
+              <TableCell>{good['Item Name']}</TableCell>
+              <TableCell>{good['Item Code']}</TableCell>
+              <TableCell className="text-right">{good['Item Quantity and Unit']}</TableCell>
             </TableRow>
           ))}
         </TableBody>
